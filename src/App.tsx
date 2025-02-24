c97fd115e10ec813c3a1154404ace16c7d181ab6
JP Wienekus <jpwienekus@gmail.com>
2025-02-24 19:46:04 +0200

    start new dashboard implementation

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,47 @@ import { ThemeProvider } from "./components/theme-provider"
 import { RevenueBreakdown } from "./pages/revenue-breakdown"
 import { Header } from "./components/header"
 import { NationalOverview } from "./pages/national-overview"
+import { useState } from "react"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./components/ui/select"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "./components/ui/tabs"
+import { NationalOverviewTab } from "./components/tabs/national-overview"
 
 function App() {
+  const [selectedYear, setSelectedYear] = useState('2023/24')
+
   return (
     <>
+      <div className="p-6 space-y-6 bg-gray-50 min-h-screen">
+        <div className="flex justify-between items-center">
+          <h1 className="text-3xl font-bold">South African Budget Analytics</h1>
+          <Select value={selectedYear} onValueChange={setSelectedYear}>
+            <SelectTrigger className="w-32">
+              <SelectValue placeholder="Year" />
+            </SelectTrigger>
+            <SelectContent>
+              {Array.from({ length: 10 }, (_, i) => (
+                <SelectItem key={`20${14 + i}/${14 + i+1}`} value={`20${14 + i}/${14 + i+1}`}>{`20${14 + i}/${14+i+1}`}</SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
+
+        <Tabs defaultValue="overview" className="w-full">
+          <TabsList className="mb-4">
+            <TabsTrigger value="overview">Overview</TabsTrigger>
+            <TabsTrigger value="revenue">Revenue Details</TabsTrigger>
+            <TabsTrigger value="expenses">Expenses Details</TabsTrigger>
+          </TabsList>
+
+          <TabsContent value="overview" className="space-y-6">
+            <NationalOverviewTab selectedYear={selectedYear}/>
+          </TabsContent>
+          <TabsContent value="revenue">test 1</TabsContent>
+          <TabsContent value="expenses">test 2</TabsContent>
+        </Tabs>
+
+
+      </div>
       <BrowserRouter>
         <ThemeProvider defaultTheme="dark">
           <div className="relative flex min-h-svh flex-col bg-background">
---
+++ src/components/tabs/national-overview.tsx
@@ -0,0 +1,104 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import revenueExpenses from '@/data/parsed/revenue-expenses.json'
+import borrowingRequirementData from '@/data/parsed/borrowing-requirement.json'
+import { useEffect, useState } from "react"
+
+type NationalOverviewTabProps = {
+  selectedYear: string
+}
+
+export const NationalOverviewTab = ({
+  selectedYear
+}: NationalOverviewTabProps) => {
+
+  const [revenue, setRevenue] = useState('')
+  const [revenueState, setRevenueState] = useState('')
+  const [expenses, setExpenses] = useState('')
+  const [expensesState, setExpensesState] = useState('')
+  const [netPosition, setNetPosition] = useState('')
+  const [netState, setNetState] = useState('')
+  const [borrowingRequirement, setBorrowingRequirement] = useState('')
+  const [borrowingRequirementState, setBorrowingRequirementState] = useState('')
+
+
+  const calculatePosition = (cPosition: number, pPosition: number) => {
+    return (((cPosition - pPosition) / pPosition) * 100).toFixed(2)
+  }
+
+  useEffect(() => {
+    const formatter = new Intl.NumberFormat('en-US', {
+      notation: 'compact',
+      compactDisplay: 'long',
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2
+    })
+
+    const currentYearIndex = revenueExpenses.findIndex(e => e.category === selectedYear)
+    const currentYear = revenueExpenses[currentYearIndex]
+    const previousYear = revenueExpenses[currentYearIndex - 1]
+    const currentYearBorrowingRequirementIndex = borrowingRequirementData.findIndex(e => e.category === selectedYear)
+    const currentYearBorrowingRequirement = borrowingRequirementData[currentYearBorrowingRequirementIndex]
+    const previousYearBorrowingRequirement = borrowingRequirementData[currentYearBorrowingRequirementIndex - 1]
+
+    setRevenue(formatter.format(currentYear.revenue).toString())
+    setRevenueState(calculatePosition(currentYear.revenue, previousYear.revenue))
+
+    setExpenses(formatter.format(currentYear.expenses).toString())
+    setExpensesState(calculatePosition(currentYear.expenses, previousYear.expenses))
+
+    const currentYearPosition = currentYear.revenue - currentYear.expenses
+    const previousYearPosition = previousYear.revenue - previousYear.expenses
+    setNetPosition(formatter.format(currentYearPosition).toString())
+    setNetState(calculatePosition(currentYearPosition, previousYearPosition))
+
+    if (currentYearBorrowingRequirement) {
+      const currentYearBorrowingRequirementTotal = currentYearBorrowingRequirement.foreign_loans + currentYearBorrowingRequirement.gfecra_settlement + currentYearBorrowingRequirement.main_budget_balance + currentYearBorrowingRequirement.domestic_long_term_loans + currentYearBorrowingRequirement.eskom_debt_relief_arrangement
+      const previousYearBorrowingRequirementTotal = previousYearBorrowingRequirement.foreign_loans + previousYearBorrowingRequirement.gfecra_settlement + previousYearBorrowingRequirement.main_budget_balance + previousYearBorrowingRequirement.domestic_long_term_loans + previousYearBorrowingRequirement.eskom_debt_relief_arrangement
+      setBorrowingRequirement(formatter.format(currentYearBorrowingRequirementTotal).toString())
+      setBorrowingRequirementState(calculatePosition(currentYearBorrowingRequirementTotal, previousYearBorrowingRequirementTotal))
+    }
+
+  }, [selectedYear])
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+      <Card>
+        <CardHeader className="pb-2">
+          <CardTitle className="text-sm font-medium">Total Revenue</CardTitle>
+          <CardContent className="px-0">
+            <div className="text-2xl font-bold">R{revenue}</div>
+            {/* no need to render -, its already being done since the number is negative */}
+            <p className={`text-xs ${+revenueState > 0 ? 'text-green-500' : 'text-red-500'}`}>{+revenueState > 0 ? '+' : ''}{revenueState}% {+revenueState > 0 ? 'up' : 'down'}</p>
+          </CardContent>
+        </CardHeader>
+      </Card>
+      <Card>
+        <CardHeader className="pb-2">
+          <CardTitle className="text-sm font-medium">Total Expenses</CardTitle>
+          <CardContent className="px-0">
+            <div className="text-2xl font-bold">R{expenses}</div>
+            <p className={`text-xs ${+expensesState > 0 ? 'text-red-500' : 'text-green-500'}`}>{+expensesState > 0 ? '+' : ''}{expensesState}% {+expensesState > 0 ? 'up' : 'down'}</p>
+          </CardContent>
+        </CardHeader>
+      </Card>
+      <Card>
+        <CardHeader className="pb-2">
+          <CardTitle className="text-sm font-medium">Net Position</CardTitle>
+          <CardContent className="px-0">
+            <div className="text-2xl font-bold">R{netPosition}</div>
+            <p className={`text-xs ${+netState > 0 ? 'text-red-500' : 'text-green-500'}`}>{+netState > 0 ? '+' : ''}{netState}% {+netState > 0 ? 'up' : 'down'}</p>
+          </CardContent>
+        </CardHeader>
+      </Card>
+      <Card>
+        <CardHeader className="pb-2">
+          <CardTitle className="text-sm font-medium">Borrowing Requirement</CardTitle>
+          <CardContent className="px-0">
+            <div className="text-2xl font-bold">R{borrowingRequirement}</div>
+            <p className={`text-xs ${+borrowingRequirementState > 0 ? 'text-red-500' : 'text-green-500'}`}>{+borrowingRequirementState > 0 ? '+' : ''}{borrowingRequirementState}% {+borrowingRequirementState > 0 ? 'up' : 'down'}</p>
+          </CardContent>
+        </CardHeader>
+      </Card>
+    </div>
+  )
+}
